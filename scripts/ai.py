f22030d5261696d42136adc14d0db6c1a90b1d27
Kyeehl <samuka_p0zz@hotmail.com>
2026-08-26 15:00:47 -0300

    Made a map move in function

--- scripts/ai.py
+++ scripts/ai.py
@@ -4,11 +4,11 @@ import numpy as np
 
 # 1 - animatronic, 2 - player, 3 - wall, 4 - door, 0 - empty space
 
-map = [[0, 0, 1, 0, 0],
-       [0, 3, 3, 3, 0],
-       [0, 3, 3, 3, 0],
-       [0, 3, 3, 3, 0],
-       [0, 4, 2, 4, 0]]
+map = np.array([[0, 0, 1, 0, 0],
+                [0, 3, 3, 3, 0],
+                [0, 3, 3, 3, 0],
+                [0, 3, 3, 3, 0],
+                [0, 4, 2, 4, 0]])
 
 ai_level = 1
 ai_move = 0
@@ -29,3 +29,28 @@ def canAiMove():
         return True
     else:
         return False
+
+def mapMove():
+    x, y = animatronic_position
+    if y > 0:
+        y -= 1
+        map[x][y] = 1
+    elif x == 0 and y == 0:
+        x += 1
+        map[x][y] = 1
+    elif x > 0:
+        x += 1
+        map[x][y] = 1
+    if x == len(map):
+        x = 0
+        y = 0
+        map[x][y] = 1
+        
+
+def game_loop():
+    while True:
+        mapMove()
+        print(map)
+        time.sleep(5000)
+
+game_loop()
